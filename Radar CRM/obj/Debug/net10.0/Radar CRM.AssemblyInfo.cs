4b19c4d67b0ddefb98f1a613e24aa65920fbdea4
eshatomar-coder <esha.tomar@bjain.in>
2026-09-24 16:35:43 +0530

    Clean up repository and ignore build files

--- Radar CRM/obj/Debug/net10.0/Radar CRM.AssemblyInfo.cs
+++ Radar CRM/obj/Debug/net10.0/Radar CRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Radar CRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1658192a06c00e75a3f0fb5c634a46d222703c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c832995e7774e0371d7a666569757e6ae0f61b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Radar CRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Radar CRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
